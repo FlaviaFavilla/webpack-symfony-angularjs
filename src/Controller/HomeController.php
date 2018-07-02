e48a10a0f6a90e2a32fa3584ce2f5a0edd4ab35c
flavia <flavia@coolshop.it>
2018-07-02 17:59:19 +0200

    locale sticky

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,14 +3,16 @@
 namespace App\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class HomeController extends BaseController
 {
     /**
      * @Route("/{_locale}/", name="homepage")
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
+        $locale = $request->getLocale();
         return $this->render("pages/index.html.twig");
     }
 }
